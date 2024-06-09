5bf5831fb618e4298ce7b41bd99a8e23d71da8b0
Stefan Dzhestanov <stefan.dzhestanov@dojo.tech>
2024-06-09 13:20:57 +0100

    Write scripts to deploy contract

---
+++ script/DeployDsc.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DscEngine} from "../src/DscEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployDsc is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (DecentralizedStableCoin, DscEngine) {
+        HelperConfig config = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            config.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DscEngine engine = new DscEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(engine));
+        vm.stopBroadcast();
+
+        return (dsc, engine);
+    }
+}
